4102e1c1996d59e36ee06c2c0aab7df7e5db4d93
Nik Yusof <nikyusof@bu.edu>
2018-07-26 00:15:56 -0400

    Updated the guestlist page for shafeeqnadia

--- webpack/guestlist.js
+++ webpack/guestlist.js
@@ -44,11 +44,13 @@ class App extends Component {
       Object.keys(guests).forEach((key) => {
         guestList.push({
           'name': guests[key].name,
+          'email': guests[key].email,
           'phone': guests[key].phone,
-          'attending': guests[key].nikah,
-          'reception': guests[key].reception,
-          'brunch': guests[key].brunch,
-          'attending': guests[key].attending
+          'nadia': guests[key].nadia,
+          'shafeeq': guests[key].shafeeq,
+          'attending': guests[key].attending,
+          'relationship': guests[key].relationship,
+          'extraGuests': guests[key].extraGuests
         });
       });
       this.setState({ data: guestList });
@@ -86,19 +88,15 @@ class App extends Component {
   render() {
     const { data, currentData } = this.state;
 
-    let nikahCount = 0;
-    let receptionCount = 0;
-    let brunchCount = 0;
+    let nadiaCount = 0;
+    let shafeeqCount = 0;
 
     data.forEach(guest => {
-      if (guest.nikah) {
-        nikahCount += parseInt(guest.attending);
+      if (guest.nadia) {
+        nadiaCount += parseInt(guest.attending);
       }
-      if (guest.reception) {
-        receptionCount += parseInt(guest.attending);
-      }
-      if (guest.brunch) {
-        brunchCount += parseInt(guest.attending);
+      if (guest.shafeeq) {
+        shafeeqCount += parseInt(guest.attending);
       }
     });
 
@@ -116,12 +114,12 @@ class App extends Component {
         Header: 'Info',
         columns: [
           {
-            Header: 'Email',
-            accessor: 'email'
-          },
-          {
             Header: 'Phone',
             accessor: 'phone'
+          },
+          {
+            Header: 'Relation',
+            accessor: 'relationship'
           }
         ]
       },
@@ -129,24 +127,16 @@ class App extends Component {
         Header: 'RSVP',
         columns: [
           {
-            Header: 'Nikah',
-            accessor: 'nikah',
+            Header: 'Nadia',
+            accessor: 'nadia',
             minWidth: MIN_WIDTH_FOR_RSVP_DETAILS,
             Cell: props => this.getRsvpDisplay(props),
             Filter: ({ filter, onChange }) => this.getRsvpFilters(filter, onChange),
             filterMethod: (filter, row) => this.filterRsvp(filter, row)
           },
           {
-            Header: 'Reception',
-            accessor: 'reception',
-            minWidth: MIN_WIDTH_FOR_RSVP_DETAILS,
-            Cell: props => this.getRsvpDisplay(props),
-            Filter: ({ filter, onChange }) => this.getRsvpFilters(filter, onChange),
-            filterMethod: (filter, row) => this.filterRsvp(filter, row)
-          },
-          {
-            Header: 'Brunch',
-            accessor: 'brunch',
+            Header: 'Shafeeq',
+            accessor: 'shafeeq',
             minWidth: MIN_WIDTH_FOR_RSVP_DETAILS,
             Cell: props => this.getRsvpDisplay(props),
             Filter: ({ filter, onChange }) => this.getRsvpFilters(filter, onChange),
@@ -162,19 +152,19 @@ class App extends Component {
     ];
 
     return (
-      <div style={{ width: '80%', margin: '3em auto' }}>
+      <div>
         <div className="row uniform" style={{ paddingBottom: '3em' }}>
-          <div className="box">
-            Nikah Count:<br />
-            <h2>{nikahCount}</h2>
+          <div className="6u 12u$(small)" style={{ textAlign: 'center' }}>
+            <div className="box">
+              Nadia Count:<br />
+              <h2>{nadiaCount}</h2>
+            </div>
           </div>
-          <div className="box">
-            Reception Count:<br />
-            <h2>{receptionCount}</h2>
-          </div>
-          <div className="box">
-            Brunch Count:<br />
-            <h2>{brunchCount}</h2>
+          <div className="6u$ 12u$(small)" style={{ textAlign: 'center' }}>
+            <div className="box">
+              Shafeeq Count:<br />
+              <h2>{shafeeqCount}</h2>
+            </div>
           </div>
         </div>
         <ReactTable
@@ -193,19 +183,25 @@ class App extends Component {
             }
           }}
           columns={columns}
-          defaultPageSize={50}
+          defaultPageSize={100}
           className='-striped -highlight'
         />
         <Modal open={this.state.open} onClose={this.onCloseModal} little>
           <div style={{textAlign: 'center', padding: '25px 15px'}}>
             <h2>Guest Details</h2>
-            <p style={{ margin: 0 }}>Name: {currentData.name}</p>
-            <p style={{ margin: 0 }}>Email: {currentData.email}</p>
-            <p style={{ margin: 0 }}>Phone: {currentData.phone}</p>
+            <p style={{ margin: 0 }}>Name: <strong>{currentData.name}</strong></p>
+            <p style={{ margin: 0 }}>Email: <a href={`mailto:${currentData.email}`}>{currentData.email}</a></p>
+            <p style={{ margin: 0 }}>Phone: <a href={`tel:${currentData.phone}`}>{currentData.phone}</a></p>
+            <p style={{ margin: 0 }}>Relation: <strong>{currentData.relationship}</strong></p>
             <p style={{ margin: 0 }}><span style={{ fontWeight: 'bold', fontStyle: 'italic' }}>{currentData.attending}</span> attending</p>
-            <p style={{ margin: 0 }}>{this.getGuestRsvpData(currentData.nikah)} Nikah</p>
-            <p style={{ margin: 0 }}>{this.getGuestRsvpData(currentData.reception)} Reception</p>
-            <p style={{ margin: 0 }}>{this.getGuestRsvpData(currentData.brunch)} Brunch</p>
+            <p style={{ margin: 0 }}>{this.getGuestRsvpData(currentData.nadia)} Nadia</p>
+            <p style={{ margin: 0 }}>{this.getGuestRsvpData(currentData.shafeeq)} Shafeeq</p>
+            {currentData.extraGuests && Object.keys(currentData.extraGuests).map(key => {
+              const index = parseInt(key) + 1;
+              return (
+                <p style={{ margin: 0 }} key={key}>Guest {index}: <strong>{currentData.extraGuests[key]}</strong></p>
+              );
+            })}
           </div>
         </Modal>
       </div>
